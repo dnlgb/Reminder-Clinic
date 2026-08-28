508ef22668c41deb54e16e2318d23fe5ac4f9103
Daniel Gonzalez <dnlsgba@gmail.com>
2026-08-27 19:14:08 -0500

    feat: boton cambia de estado callback

--- src/PendingCallbacks.tsx
+++ src/PendingCallbacks.tsx
@@ -1,16 +1,47 @@
+import { useState } from "react"
 
 function PendingCallbacks() {
-    const callbacks = [
+    const [callbacks, setCallbacks] = useState(
+        [
         {
-            id: 1, patient: "dan", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd"
+            id: 1, patient: "dan", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd",
+            status: "pending"
         },
         {
-            id: 2, patient: "juan", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd"
+            id: 2, patient: "juan", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd",
+            status: "pending"
         },
         {
-            id: 3, patient: "meme", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd"
+            id: 3, patient: "meme", date: "2026-10-12 10:30", reason: "lorem ipsum xdxd",
+            status: "pending"
         }
-    ]
+    ])
+    const showCallback = (callback: {
+        id: number
+        patient: string
+        date: string
+        reason: string
+        }) => {
+            console.log(callback)
+        }
+
+    const handleComplete = (callbackComplete: {
+        id: number
+        patient: string
+        date: string
+        reason: string
+        status: string
+    }) => {
+        setCallbacks(
+            callbacks.map((currentCallback) => {
+            if(currentCallback.id === callbackComplete.id) {
+                return {...currentCallback, status: "completed" }
+            }else{
+                return currentCallback
+            }
+            })
+        )
+    }
     return(
         <section>
             <h2>Pending</h2>
@@ -21,7 +52,13 @@ function PendingCallbacks() {
                     <span>{callback.patient}</span>
                     <strong>{callback.date}</strong>
                     <span>{callback.reason}</span>
-                    <button type="button">Boton</button>
+                    <span>{callback.status}</span>
+                    <button 
+                    type="button"
+                    onClick={() => {
+                        handleComplete(callback)
+                    }}
+                    >Boton</button>
                 </div>
                 ))}
             </div>    
