4c4b67fb78ddf4ffa00189f03e01b85ae463396d
Daniel Gonzalez <dnlsgba@gmail.com>
2026-09-24 22:01:37 -0500

    feat: nuevo estilo sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Sidebar() {
     return(
         <aside className="sidebar">
-            <div>
-            <h2>Callback Clinic</h2>
+            <div className="sidebar-brand">
+                <div className="sidebar-logo">
+                    ↪
+                </div>
+
+                <h2>Callback Clinic</h2>
+            </div>
+
             <nav className="sidebar-nav">
-                <Link to="/">Dashboard</Link>
-                <Link to="/clients">Clients</Link>
-                <Link to="/callbacks">Callbacks</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/clients">Clients</NavLink>
+                <NavLink to="/callbacks">Callbacks</NavLink>
             </nav>
+
+            <div className="sidebar-user">
+                <span>EV</span>
+                <div>
+                    <strong>Dr. Elena Vance</strong>
+                    <small>Care Coordinator</small>
+                </div>
             </div>
-            <div>
-                <a href="" className="sidebar-user">User</a>
-            </div>
+
             
         </aside>
     )
